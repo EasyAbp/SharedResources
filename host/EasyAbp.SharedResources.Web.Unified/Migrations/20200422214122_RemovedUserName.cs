e2c9412ff8d10c3f24f67b476a20a3e81cb97e4e
gdlcf88 <47396430@qq.com>
2020-04-23 05:44:54 +0800

    Removed UserName

---
+++ host/EasyAbp.SharedResources.Web.Unified/Migrations/20200422214122_RemovedUserName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyAbp.SharedResources.Migrations
+{
+    public partial class RemovedUserName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "SharedResourcesResourceUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "SharedResourcesResourceUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
