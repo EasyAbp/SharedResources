d6d55ca2548231a082e2e0d62d743d26d489e4ed
gdlcf88 <gdlcf88@outlook.com>
2020-04-23 12:26:57 +0800

    Removed the GetPolicyName and GetListPolicyName in CategoryAppService.cs

--- src/EasyAbp.SharedResources.Application/EasyAbp/SharedResources/Categories/CategoryAppService.cs
+++ src/EasyAbp.SharedResources.Application/EasyAbp/SharedResources/Categories/CategoryAppService.cs
@@ -15,8 +15,6 @@ namespace EasyAbp.SharedResources.Categories
         protected override string CreatePolicyName { get; set; } = SharedResourcesPermissions.Categories.Create;
         protected override string DeletePolicyName { get; set; } = SharedResourcesPermissions.Categories.Delete;
         protected override string UpdatePolicyName { get; set; } = SharedResourcesPermissions.Categories.Update;
-        protected override string GetPolicyName { get; set; } = SharedResourcesPermissions.Categories.Default;
-        protected override string GetListPolicyName { get; set; } = SharedResourcesPermissions.Categories.Default;
         
         private readonly ICategoryRepository _repository;
 
