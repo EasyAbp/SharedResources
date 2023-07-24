5fe9349b1f6110814148720cac335334380eb248
gdlcf88 <gdlcf88@outlook.com>
2023-07-25 02:25:50 +0800

    Upgrade to ABP 7.3.0

--- src/EasyAbp.SharedResources.Domain.Shared/EasyAbp/SharedResources/SharedResourcesDomainSharedModule.cs
+++ src/EasyAbp.SharedResources.Domain.Shared/EasyAbp/SharedResources/SharedResourcesDomainSharedModule.cs
@@ -1,4 +1,5 @@
-﻿using Volo.Abp.Modularity;
+﻿using Volo.Abp.Domain;
+using Volo.Abp.Modularity;
 using Volo.Abp.Localization;
 using EasyAbp.SharedResources.Localization;
 using Volo.Abp.Localization.ExceptionHandling;
@@ -9,7 +10,8 @@ using Volo.Abp.VirtualFileSystem;
 namespace EasyAbp.SharedResources
 {
     [DependsOn(
-        typeof(AbpValidationModule)
+        typeof(AbpValidationModule),
+        typeof(AbpDddDomainSharedModule)
     )]
     public class SharedResourcesDomainSharedModule : AbpModule
     {
